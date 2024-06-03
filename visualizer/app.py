01e291d27e927f9419f4d5d6a1974fe7a7ffb74d
Yuxuan Hu <lydiahu_hn@hotmail.com>
2024-06-03 15:01:25 +1000

    refactor code

--- visualizer/app.py
+++ visualizer/app.py
@@ -1,36 +1,30 @@
-import io
-import matplotlib.pyplot as plt
-import networkx as nx
-from flask import Flask, Response
-import pickle
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import random
 
 app = Flask(__name__)
-
-@app.route('/graph')
-def plot_graph():
-    # Load the graph from a pickle file
-    with open('../saved/G.pickle', 'rb') as f:
-        G = pickle.load(f)
-
-    # Draw the graph
-    fig, ax = plt.subplots(figsize=(6, 4))
-    nx.draw(G, ax=ax, with_labels=True)
-
-    # Save the plot to a BytesIO buffer
-    img_buffer = io.BytesIO()
-    plt.savefig(img_buffer, format='png')
-    plt.close(fig)
-    img_buffer.seek(0)
-
-    # Serve the image
-    return Response(img_buffer, mimetype='image/png')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def home():
-    return '''
-        <h1>NetworkX Graph from Pickle</h1>
-        <img src="/graph" alt="Graph"/>
-    '''
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    emit('log', {'msg': 'Welcome! Waiting for news to propagate...'})
+
+@socketio.on('share_news')
+def handle_share_news(data):
+    print(f"News received: {data['msg']}")
+    # Simulate the propagation probability
+    if random.random() < 0.5:  # 50% chance to propagate the news
+        emit('news', {'msg': data['msg']}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
