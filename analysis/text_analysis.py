d61c7a76b32b4d6db7b7950242c856eb7d81012f
lindsay0416 <lindsay.z14353@gmail.com>
2024-07-01 21:55:03 +1200

    text analysis

---
+++ analysis/text_analysis.py
@@ -0,0 +1,4 @@
+# Clustering: https://scikit-learn.org/stable/modules/clustering.html#clustering
+# Use LLM to extract the aspect and opinion.
+# Use prompt engineering to improve the accuracy of LLM's output.
+
